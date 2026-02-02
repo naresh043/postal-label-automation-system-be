8ccaae2e48ab22e1f33faaf17e13a4e47cfc19d4
NareshSanjeev <naresh732003@gmail.com>
2026-02-02 14:29:58 +0530

    feat: env added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://NamasteNaresh:Naresh%40143@namastenaresh.qddqpyv.mongodb.net/postalLabelsPrinting");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Error", error);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// require("dotenv").config();
+require("dotenv").config();
 
 const connectDB = require("./config/db");
 
